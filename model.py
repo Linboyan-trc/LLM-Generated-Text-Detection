770374391d1c2b5bbc8f360c1bf3ba296bb4d0a4
Toryn <1584340372@qq.com>
2025-05-26 07:01:46 +0800

    update

--- model.py
+++ model.py
@@ -8,50 +8,11 @@ from transformers import (
     AutoModelForSequenceClassification, 
     Trainer, 
     TrainingArguments,
-    EarlyStoppingCallback,
-    AutoConfig
+    EarlyStoppingCallback
 )
 from sklearn.metrics import f1_score
 from tqdm import tqdm
-from transformers import AutoModel, PreTrainedModel
-from transformers.modeling_outputs import SequenceClassifierOutput
-from torch import nn
 
-class CustomBertForSequenceClassification(PreTrainedModel):
-    def __init__(self, config):
-        super().__init__(config)
-        self.bert = AutoModel.from_config(config)
-        self.dropout = nn.Dropout(config.hidden_dropout_prob)
-        self.classifier = nn.Linear(config.hidden_size, config.num_labels)
-        self.config = config
-
-    def forward(self, input_ids=None, attention_mask=None, token_type_ids=None, labels=None):
-        outputs = self.bert(
-            input_ids=input_ids,
-            attention_mask=attention_mask,
-            token_type_ids=token_type_ids,
-            return_dict=True
-        )
-        last_hidden_state = outputs.last_hidden_state
-        # 混合池化：mean pooling + [CLS]
-        mean_pooling = torch.sum(last_hidden_state * attention_mask.unsqueeze(-1), dim=1) / attention_mask.sum(dim=1, keepdim=True)
-        cls_token = last_hidden_state[:, 0]
-        pooled_output = 0.5 * mean_pooling + 0.5 * cls_token
-
-        pooled_output = self.dropout(pooled_output)
-        logits = self.classifier(pooled_output)
-
-        loss = None
-        if labels is not None:
-            loss_fct = nn.CrossEntropyLoss()
-            loss = loss_fct(logits.view(-1, self.config.num_labels), labels.view(-1))
-
-        return SequenceClassifierOutput(
-            loss=loss,
-            logits=logits,
-            hidden_states=outputs.hidden_states,
-            attentions=outputs.attentions
-        )
 
 ############################## 1. 训练 ##############################
 # 1.1 数据加载
@@ -76,9 +37,7 @@ def get_datasets(train_path, dev_path):
 # 2. 拿到预训练的模型
 def get_model_and_tokenizer(model_name='hfl/chinese-macbert-large'):
     tokenizer = AutoTokenizer.from_pretrained(model_name)
-    config = AutoConfig.from_pretrained(model_name)
-    config.num_labels = 2
-    model = CustomBertForSequenceClassification.from_pretrained(model_name, config=config)
+    model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=2)
     return tokenizer, model
 
 
